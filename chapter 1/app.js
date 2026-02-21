fe1c97963c2648d170a554a34390396cd78efc1c
syeda9166 <syedahania210@gmail.com>
2026-02-21 12:58:00 +0500

    commit

--- chapter 1/app.js
+++ chapter 1/app.js
@@ -1,3 +1,11 @@
 // alert("hello ");
 
-alert("please enter a valid password");
+// alert("please enter a valid password");
+
+// alert("welcome to js land... \n happy coding!");
+
+// alert("welcome to js land...");
+
+// alert("Happy coding ");
+
+// alert("Hello ... i can run js through my web browser's console");
