e5aad56803e761cae0572bbe224ed988db70d7e5
syedahania210@gmail.com <syedahania210@gmail.com>
2026-03-08 01:46:23 +0500

    commit

--- chapter 5/app.js
+++ chapter 5/app.js
@@ -26,18 +26,51 @@
 // document .write( `sum of ${ num1} and ${num2 } is ${sum}`)
  
                         //  DIVISION 
- let num1 =+prompt("enter your number ")
-let num2 =+prompt(" enter your number ")
+//  let num1 =+prompt("enter your number ")
+// let num2 =+prompt(" enter your number ")
 
-let sum =num1/num2
+// let sum =num1/num2
 
-document .write( `sum of ${ num1} and ${num2 } is ${sum}`)
+// document .write( `sum of ${ num1} and ${num2 } is ${sum}`)
 
 //  QUESTION 2 END 
 
-
 //   QUESTION 3 START 
 
+//  A
 
+// let num
+
+// // B 
+
+// document .write(` value of variable after declaration  is ${num }`)
+
+// document.write(" <br/>")
+// // C 
+
+// num = 5;
+
+// //  D
+// document .write(` value of variable after declaration  is ${num }`)
+// document.write(" <br/>")
+// //  E 
+// num++
+// // F
+// document .write(` value of variable after declaration  is ${num }`)
+// document.write(" <br/>")
+
+// num = num +7
+// // g
+// document .write(` value of variable after declaration  is ${num } `)
+// document.write(" <br/>")
 
 //  QUESTION 3 END 
+
+// QUESTION 4 START 
+let ticket= 600
+
+let fiveticket= 600*5
+
+document. write(`Total cost to buy 5 tickets to a movie is ${fiveticket}PKR` )
+
+//  QUESTION 4 END 
