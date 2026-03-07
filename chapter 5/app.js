b0de501f1125e125460396058a2a0fc4a474dde2
syedahania210@gmail.com <syedahania210@gmail.com>
2026-03-08 01:56:18 +0500

    commit

--- chapter 5/app.js
+++ chapter 5/app.js
@@ -67,10 +67,53 @@
 //  QUESTION 3 END 
 
 // QUESTION 4 START 
-let ticket= 600
+// let ticket= 600
 
-let fiveticket= 600*5
+// let fiveticket= 600*5
 
-document. write(`Total cost to buy 5 tickets to a movie is ${fiveticket}PKR` )
+// document. write(`Total cost to buy 5 tickets to a movie is ${fiveticket}PKR` )
 
 //  QUESTION 4 END 
+
+
+// QUESTION 5 START 
+
+let count= 1; 
+let tablenumber=4
+
+document.write(" TABLE OF 4 ")
+
+document.write("<br/>")
+
+document.write(`${tablenumber} x ${count}= ${tablenumber}`) 
+document.write("<br/>")
+
+count++
+document.write(`${tablenumber} x ${count}= ${tablenumber *count}`)
+document.write("<br/>")
+count++
+document.write(`${tablenumber} x ${count}= ${tablenumber *count}`)
+document.write("<br/>")
+count++
+document.write(`${tablenumber} x ${count}= ${tablenumber *count}`)
+document.write("<br/>")
+count++
+document.write(`${tablenumber} x ${count}= ${tablenumber *count}`)
+document.write("<br/>")
+count++
+document.write(`${tablenumber} x ${count}= ${tablenumber *count}`)
+document.write("<br/>")
+count++
+document.write(`${tablenumber} x ${count}= ${tablenumber *count}`)
+document.write("<br/>")
+count++
+document.write(`${tablenumber} x ${count}= ${tablenumber *count}`)
+document.write("<br/>")
+count++
+document.write(`${tablenumber} x ${count}= ${tablenumber *count}`)
+document.write("<br/>")
+count++
+document.write(`${tablenumber} x ${count}= ${tablenumber *count}`)
+document.write("<br/>")
+
+//  QUESTION 5 END 
