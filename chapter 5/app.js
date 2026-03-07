f35ef36a432230505834837d90110a0e7a9093b4
syedahania210@gmail.com <syedahania210@gmail.com>
2026-03-08 01:07:47 +0500

    commit

--- chapter 5/app.js
+++ chapter 5/app.js
@@ -9,11 +9,35 @@
 //  question 1 end 
 
 // QUESTION 2 START 
+                    //   SUBTRACTION
+//  let num1 =+prompt("enter your number ")
+// let num2 =+prompt(" enter your number ")
+
+// let sum =num1- num2
+
+// document .write( `sum of ${ num1} and ${num2 } is ${sum}`)
+
+                      //  MULTIPLICATION 
+// let num1 =+prompt("enter your number ")
+// let num2 =+prompt(" enter your number ")
+
+// let sum =num1* num2
+
+// document .write( `sum of ${ num1} and ${num2 } is ${sum}`)
+ 
+                        //  DIVISION 
  let num1 =+prompt("enter your number ")
 let num2 =+prompt(" enter your number ")
 
-let sum =num1- num2
+let sum =num1/num2
 
 document .write( `sum of ${ num1} and ${num2 } is ${sum}`)
 
 //  QUESTION 2 END 
+
+
+//   QUESTION 3 START 
+
+
+
+//  QUESTION 3 END 
