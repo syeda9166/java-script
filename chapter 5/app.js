83f705336cdeadc9d1d2e299b49e3d93761d3f55
syedahania210@gmail.com <syedahania210@gmail.com>
2026-03-08 00:48:51 +0500

    commit

--- chapter 5/app.js
+++ chapter 5/app.js
@@ -1,9 +1,19 @@
 //  question 1 
  
+//  let num1 =+prompt("enter your number ")
+// let num2 =+prompt(" enter your number ")
+
+// let sum =num1+ num2
+
+// document .write( `sum of ${ num1} and ${num2 } is ${sum}`)
+//  question 1 end 
+
+// QUESTION 2 START 
  let num1 =+prompt("enter your number ")
 let num2 =+prompt(" enter your number ")
 
-let sum =num1+ num2
+let sum =num1- num2
 
 document .write( `sum of ${ num1} and ${num2 } is ${sum}`)
-//  question 1 end 
+
+//  QUESTION 2 END 
