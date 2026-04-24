8035b227c8c8669a3f1353acef028169546db3a5
syedahania210@gmail.com <syedahania210@gmail.com>
2026-04-24 17:38:47 +0500

    commit

---
+++ chapter 17-20/app.js
@@ -0,0 +1,22 @@
+                                    //   CHAPTER 17-20 
+                                    // 
+                                    // QUESTION # 1  
+
+// i[][] arr = new int[3][4]; 
+
+                                //    QUESTION 2 
+
+//   i [][] matrix = {
+//     {1, 2, 3}
+//     {4, 5, 6}
+//     {7, 8, 9}
+// };
+                              
+                            //   QUESTION 3
+//   for ( i = 1; i <= 10; i++){
+//     console.log(i) 
+//   }                          
+                          
+                            //   QUESTION 4 
+
+                            
