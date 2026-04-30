4de61a5c46396aefd838e8ed94730bd4eb2a3aa9
syedahania210@gmail.com <syedahania210@gmail.com>
2026-04-30 20:25:15 +0500

    commit

---
+++ chapter 21-25/app.js
@@ -0,0 +1,20 @@
+                                // CHAPTER 21 TOO 25  
+
+                                //  QUESTION 1 
+
+
+// let firstName = prompt("Enter your first name:");
+// let lastName = prompt("Enter your last name:");
+
+// let fullName = firstName +  lastName;
+
+// alert("Hello " + fullName );
+
+
+
+                        //  QUESTION 2 
+// let mobileModel = prompt("Enter your favorite mobile phone model");
+
+// let lengthOfInput = mobileModel.length;
+
+// console.log("Length of your input is      " + lengthOfInput);
